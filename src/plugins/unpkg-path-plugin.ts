200b57144bec2795bc91fc34d8ccc58676513baf
minhtungo <loducfc@gmail.com>
2022-11-11 22:24:27 -0500

    imnplement caching layer

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache',
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -40,12 +45,27 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        //Check to see if already fetched this file and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+
+        //store data in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
