12521a48a56fd8a7d44b6d1a3fffcb00658b3dcd
minhtungo <loducfc@gmail.com>
2022-11-14 18:29:12 -0500

    add deboucing logic

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './CodeEditor';
 import Preview from './Preview';
 import bundler from '../bundler';
@@ -8,10 +8,16 @@ const CodeCell = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = async () => {
-    const output = await bundler(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundler(input);
+      setCode(output);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction='vertical'>
