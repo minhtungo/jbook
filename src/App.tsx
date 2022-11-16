a7272551c3e50bc2f40fe37cf3cf921717f0ebc6
minhtungo <loducfc@gmail.com>
2022-11-16 02:38:10 -0500

    typed selector, action creator helper

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import CellList from './components/cell-list/CellList';
 // import CodeCell from './components/code-cell/CodeCell';
-import TextEditor from './components/text-editor/TextEditor';
 
 const App = () => {
   return (
     <div>
-      {/* <CodeCell /> */}
-      <TextEditor />
+      <CellList />
     </div>
   );
 };
---
+++ src/components/cell-list-item/CellListItem.tsx
@@ -0,0 +1,19 @@
+import { Cell } from '../../redux';
+import CodeCell from '../code-cell/CodeCell';
+import TextEditor from '../text-editor/TextEditor';
+
+interface CellListItemProps {
+  cell: Cell;
+}
+
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  let child: JSX.Element;
+  if (cell.type === 'code') {
+    child = <CodeCell />;
+  } else {
+    child = <TextEditor />;
+  }
+
+  return <div>{child}</div>;
+};
+export default CellListItem;
