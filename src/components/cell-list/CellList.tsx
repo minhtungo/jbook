b462a0e1670b83af088d110ca7efb090339a8580
minhtungo <loducfc@gmail.com>
2022-11-16 19:02:04 -0500

    implement and style Add Cell

--- src/components/cell-list/CellList.tsx
+++ src/components/cell-list/CellList.tsx
@@ -1,5 +1,7 @@
 import { useTypedSelector } from '../../hooks/useTypedSelector';
+import AddCell from '../add-cell/AddCell';
 import CellListItem from '../cell-list-item/CellListItem';
+import { Fragment } from 'react';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -7,8 +9,16 @@ const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
 export default CellList;
