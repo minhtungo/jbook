523c6cf46c446d34154342f38158811843b6e006
minhtungo <loducfc@gmail.com>
2022-11-18 00:25:26 -0500

    implement loading bar

--- src/components/code-cell/CodeCell.tsx
+++ src/components/code-cell/CodeCell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css';
 import { useEffect } from 'react';
 import CodeEditor from '../code-editor/CodeEditor';
 import Preview from '../preview/Preview';
@@ -44,8 +45,15 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        {!bundle || bundle.loading ? (
+          <div className='progress-cover'>
+            <progress className='progress is-small is-primary' max='100'>
+              Loading
+            </progress>
+          </div>
+        ) : (
+          <Preview code={bundle.code} err={bundle.err} />
+        )}
       </div>
     </Resizable>
   );
