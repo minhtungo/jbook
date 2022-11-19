eda40f75875bd4d14378a0ca937ade4efcd10f23
minhtungo <loducfc@gmail.com>
2022-11-19 00:35:30 -0500

    implement showing React components

--- src/components/code-cell/CodeCell.tsx
+++ src/components/code-cell/CodeCell.tsx
@@ -21,15 +21,18 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     //Add a show function to conveniently render on the Preview
     const cumulativeCode = [
       `
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
         const show = (value) => {
+          const root = document.querySelector('#root')
           if(typeof value === 'object') {
             if(value.$$typeof && value.props) {
-              ReactDOM.render(value, document.querySelector('#root))
+              _ReactDOM.render(value, root)
             } else {
-              document.querySelector('#root).innerHTML = JSON.stringify(value);
+              root.innerHTML = JSON.stringify(value);
             }
           } else {
-            document.querySelector('#root).innerHTML = value;
+            root.innerHTML = value;
           }
         }
       `,
